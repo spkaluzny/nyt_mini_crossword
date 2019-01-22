3adfc75128211caab8b1f414bab45ae05afb39da
spkal <spk@seapowet.org>
2019-01-21 18:15:35 -0800

    Updated data.

--- nyttime.R
+++ nyttime.R
@@ -2,10 +2,12 @@ library("lubridate")
 library("dplyr")
 library("ggplot2")
 d <- read.csv("nyt.csv", stringsAsFactors=FALSE)
+d <- d %>%
+  filter(Player %in% c("SPK", "JAK", "JIK"))
 d$Date <- as.Date(d$Date)
 d$WeekDay <- lubridate::wday(d$Date)
 d$Seconds <- with(d, seconds(ms(Time)))
 d %>%
   ggplot(aes(x=WeekDay, y=Seconds)) +
   geom_point() +
-  facet_wrap( ~ Player)
+  facet_grid(Player ~ .)
